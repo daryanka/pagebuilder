33b4b88d3520e3f26614191a049a69fd937d5c86
Daryan <daryanka@hotmail.co.uk>
2020-08-08 00:46:36 +0100

    added left side + css

--- src/Containers/LeftPanel/LeftPanel.js
+++ src/Containers/LeftPanel/LeftPanel.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Card from "../../Components/Cards/Card";
 import {ImageType, TextType, ThreeDroppableColumns, TwoDroppableColumns} from "../../CardTypes";
+import {AiOutlineApartment, GrNewWindow, RiSettings5Line} from "react-icons/all";
 
 const CardList = [
   {
@@ -24,14 +25,30 @@ const CardList = [
 const LeftPanel = (props) => {
   return (
     <div className={"left-panel"}>
-      <h1>Components</h1>
-      <div className={"components-list"}>
-        {CardList.map((el, i) => (
-          <Card
-            key={el.type}
-            {...el}
-          />
-        ))}
+      <div className="left-side">
+        <div className={"item"}>
+          <AiOutlineApartment/>
+          <p>Sections</p>
+        </div>
+        <div className={"item"}>
+          <GrNewWindow/>
+          <p>Add Section</p>
+        </div>
+        <div className="item">
+          <RiSettings5Line/>
+          <p>Settings</p>
+        </div>
+      </div>
+      <div className="component-list-wrapper">
+        <h2>Components</h2>
+        <div className={"components-list"}>
+          {CardList.map((el, i) => (
+            <Card
+              key={el.type}
+              {...el}
+            />
+          ))}
+        </div>
       </div>
     </div>
   )
